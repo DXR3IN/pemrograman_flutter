e6a772b5dc2b600504d8715f04cd63dcedcf22cc
DXR3IN <mhdirfan0856@gmail.com>
2023-10-18 19:28:58 +0700

    Update

--- navigation_2201082012/lib/pages/page_dua.dart
+++ navigation_2201082012/lib/pages/page_dua.dart
@@ -3,25 +3,23 @@ import 'package:flutter/material.dart';
 class PageTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("NAVIGATION"),
-        ),
-        body: Center(
-          child: Text(
-            "Ini page 2",
-            style: TextStyle(fontSize: 25),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Icon(Icons.keyboard_arrow_left),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NAVIGATION"),
       ),
+      body: Center(
+        child: Text(
+          "Ini page 2",
+          style: TextStyle(fontSize: 25),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        child: Icon(Icons.keyboard_arrow_left),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
