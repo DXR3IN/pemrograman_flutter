354b2d565b1b455149d72eb4284eec216d862401
DXR3IN <mhdirfan0856@gmail.com>
2023-10-29 00:32:35 +0700

    Update main.dart

--- cupertino_2201082012/lib/main.dart
+++ cupertino_2201082012/lib/main.dart
@@ -26,46 +26,67 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return Platform.isIOS
-                    ? CupertinoAlertDialog(
-                        title: Text("DELETE"),
-                        content: Text("Are you sure to delete this?"),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Text("cancel"),
-                          ),
-                          TextButton(
-                            onPressed: () {},
-                            child: Text("Oke"),
-                          )
-                        ],
-                      )
-                    : AlertDialog(
-                        title: Text("DELETE"),
-                        content: Text("Are you sure to delete this?"),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Text("cancel"),
-                          ),
-                          TextButton(
-                            onPressed: () {},
-                            child: Text("Oke"),
-                          )
-                        ],
+            //   showDialog(
+            //     context: context,
+            //     builder: (context) {
+            //       return Platform.isIOS
+            //           ? CupertinoAlertDialog(
+            //               title: Text("DELETE"),
+            //               content: Text("Are you sure to delete this?"),
+            //               actions: [
+            //                 TextButton(
+            //                   onPressed: () {
+            //                     Navigator.of(context).pop();
+            //                   },
+            //                   child: Text("cancel"),
+            //                 ),
+            //                 TextButton(
+            //                   onPressed: () {},
+            //                   child: Text("Oke"),
+            //                 )
+            //               ],
+            //             )
+            //           : AlertDialog(
+            //               title: Text("DELETE"),
+            //               content: Text("Are you sure to delete this?"),
+            //               actions: [
+            //                 TextButton(
+            //                   onPressed: () {
+            //                     Navigator.of(context).pop();
+            //                   },
+            //                   child: Text("cancel"),
+            //                 ),
+            //                 TextButton(
+            //                   onPressed: () {},
+            //                   child: Text("Oke"),
+            //                 )
+            //               ],
+            //             );
+            //     },
+            //   );
+            Platform.isAndroid
+                ? showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2003),
+                    lastDate: DateTime(2050),
+                    onDatePickerModeChange: (value) {
+                      print(value);
+                    },
+                  )
+                : showCupertinoModalPopup(
+                    context: context,
+                    builder: (context) {
+                      return CupertinoDatePicker(
+                        onDateTimeChanged: (datetime) {
+                          print(datetime);
+                        },
+                        initialDateTime: DateTime.now(),
                       );
-              },
-            );
+                    },
+                  );
           },
-          child: Text("DELETE"),
+          child: Text("Tanggal"),
         ),
       ),
     );
